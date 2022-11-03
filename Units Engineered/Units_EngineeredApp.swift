6e469e9c8e36373f8ee128bc88358c2db8944cb5
Camden Carlson <ccarlson43@gatech.edu>
2022-11-02 19:58:56 -0600

    Initial Commit

---
+++ Units Engineered/Units_EngineeredApp.swift
@@ -0,0 +1,17 @@
+//
+//  Units_EngineeredApp.swift
+//  Units Engineered
+//
+//  Created by Barry Carlson on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Units_EngineeredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

3d5e9d920d743d08559aee1d6eba67913451ed83
Camden Carlson <ccarlson43@gatech.edu>
2022-11-12 15:00:08 -0700

    .onTapGesture() to minimize keyboard, heat transfer coefficient and thermal conduction functionality

--- Units Engineered/ContentView.swift
+++ Units Engineered/ContentView.swift
@@ -20,17 +20,31 @@ struct ContentView: View {
                 RoundedRectangle(cornerRadius: 20)
                     .frame(width: 300.0, height: 300.0)
                     .foregroundColor(Color(UIColor.systemBackground))
+                    .opacity(0)
+                
+                    
                 HStack { // values and units
                     VStack(alignment: .leading) { // Names of units
                         TextView(content: viewModel.units[viewModel.options.firstIndex(of: viewModel.selection) ?? 0])
+                            
                     }
+                    
+                    
                     VStack { // Values text boxes
                         ValueView(output: viewModel.output())
                     }
                 }
             }
             .padding()
+            .onTapGesture {
+                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+            }
             // End of Answer platform/area
+            
+            
+            
+            
+            
             
             
                 
@@ -39,7 +53,7 @@ struct ContentView: View {
                 Spacer()
                 ZStack {
                     RoundedRectangle(cornerRadius: 20)
-                        .frame(width: 150.0, height: 70.0)
+                        .frame(width: 150.0, height: 40.0)
                         .foregroundColor(Color(UIColor.systemBackground))
                     Picker("Select Measurement", selection: $viewModel.selection) {
                         ForEach(viewModel.options, id: \.self) {
@@ -93,6 +107,8 @@ struct TextView: View {
     var body: some View {
         ForEach(content[0..<content.count], id: \.self) { aContent in
             Text(aContent)
+                .foregroundColor(Color(UIColor.systemBackground))
+                .colorInvert()
                 .frame(height: 18)
                 .font(.system(size: 25))
             
@@ -105,6 +121,8 @@ struct ValueView: View {
     var body: some View {
         ForEach(output, id: \.self) { out in
             Text(out)
+                .foregroundColor(Color(UIColor.systemBackground))
+                .colorInvert()
                 .frame(height: 18)
                 .font(.system(size: 23))
                 

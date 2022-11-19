d03a66321dfd3edeba3bd86ef8ece8f879ba806b
Camden Carlson <ccarlson43@gatech.edu>
2022-11-19 18:14:34 -0500

    unit selector is wider

--- Units Engineered/CalcView.swift
+++ Units Engineered/CalcView.swift
@@ -107,7 +107,7 @@ struct CalcView: View {
                     }
                 
                 }
-                .frame(width: 85, height: 30)
+                .frame(width: 100, height: 30)
                 .fixedSize(horizontal: false, vertical: true)
                 .padding(.all, 10)
                 
